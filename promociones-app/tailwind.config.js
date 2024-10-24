0c2d0b099ff07de14284557f1b233f53fc6c1841
Brandon F <168985205+Alafa117@users.noreply.github.com>
2024-10-24 16:15:56 -0500

    Front funcionando FUll

--- promociones-app/tailwind.config.js
+++ promociones-app/tailwind.config.js
@@ -5,12 +5,7 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#3B82F6', // blue-500
-        secondary: '#10B981', // green-500
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
